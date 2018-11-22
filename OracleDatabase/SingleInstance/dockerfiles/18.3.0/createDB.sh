bd64a9dacc0eb1814a557b8b8d288292662a8897
Milos Kleint <mkleint@atlassian.com>
2018-11-23 10:35:06 +1100

    hardcode password for 18.3 as well

--- OracleDatabase/SingleInstance/dockerfiles/18.3.0/createDB.sh
+++ OracleDatabase/SingleInstance/dockerfiles/18.3.0/createDB.sh
@@ -22,7 +22,9 @@ export ORACLE_SID=${1:-ORCLCDB}
 export ORACLE_PDB=${2:-ORCLPDB1}
 
 # Auto generate ORACLE PWD if not passed on
-export ORACLE_PWD=${3:-"`openssl rand -base64 8`1"}
+# export ORACLE_PWD=${3:-"`openssl rand -base64 8`1"}
+# hardcode password if not provided
+export ORACLE_PWD=${ORACLE_PWD:-oracle}
 echo "ORACLE PASSWORD FOR SYS, SYSTEM AND PDBADMIN: $ORACLE_PWD";
 
 # Replace place holders in response file
